210fe20aaceaf741e25a5ffb93c5885c91eeb367
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-09 04:55:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array = ["wow", "I", "am", "really", "learning"] << "arrays!"
+  add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array = ["I", "am", "really", "learning"]
-  create_an_array.unshift("wow")
+  add_element_to_start_of_array = ["I", "am", "really", "learning"]
+  cadd_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
@@ -21,18 +21,21 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  create_an_array.shift
+  remove_element_from_start_of_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  remove_element_from_start_of_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_element_from_index = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_element_from_index[2]
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array[0]
+  retrieve_first_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_first_element_from_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  create_an_array[-1]
+  retrieve_last_element_from_array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  retrieve_last_element_from_array[-1]
 end
