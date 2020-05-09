e605218dee62c6548bf2b6e2227c099bafd3cece
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-09 04:56:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   add_element_to_start_of_array = ["I", "am", "really", "learning"]
-  cadd_element_to_start_of_array.unshift("wow")
+  add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
